fd7fbe0d8f10562a7f8850b5a0febccfd7c20d7b
nymphinthetrap <hanayusy@gmail.com>
2025-04-26 12:53:28 +0100

    Scoreboard

---
+++ Scrim-s04k019/index.js
@@ -0,0 +1,36 @@
+let homeScoreEl =document.getElementById("home-score")
+let homeScore = 0
+
+function increaseHomeScoreOne(){
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
+}
+
+function increaseHomeScoreTwo(){
+    homeScore += 2
+    homeScoreEl.textContent = homeScore
+}
+
+function increaseHomeScoreThree(){
+    homeScore += 3
+    homeScoreEl.textContent = homeScore
+}
+
+
+let guestScoreEl =document.getElementById("guest-score")
+let guestScore = 0
+
+function increaseGuestScoreOne(){
+    guestScore += 1
+    guestScoreEl.textContent = guestScore
+}
+
+function increaseGuestScoreTwo(){
+    guestScore += 2
+    guestScoreEl.textContent = guestScore
+}
+
+function increaseGuestScoreThree(){
+    guestScore += 3
+    guestScoreEl.textContent = guestScore
+}
